f3896830813eec2ee41d3d131c2a4e551e5c19cf
Jesse Theisen <theiseja@gmail.com>
2019-03-06 21:53:39 -0600

    MVP: Added env file and dep

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const server = require('./api/server.js');
-PORT = process.env.PORT || 9090
+PORT = process.env.PORT || 9090;
 
 server.listen(PORT, () => {
   console.log('\n*** Server Running on PORT ${PORT} ***\n');
